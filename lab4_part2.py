289b82fa8b101ac9328a558302abfd6062e429a5
BacCM <baccm@mail.ru>
2026-06-14 23:35:07 +0300

    lab4 part 2 Issue 14_2

--- lab4_part2.py
+++ lab4_part2.py
@@ -9,3 +9,11 @@ sns.set_theme(style="darkgrid")
 # Задание 2.14. Оценить исходный датафрейм df на наличие выбросов по отдельным параметрам
 sns.boxplot(x=df["Growth"])
 plt.show()
+
+# Задание 14.2 - оценить параметр вес
+sns.boxplot(y=df["Weight"])
+plt.show()
+
+# Задание 14.3 - оценить параметр возраст самостоятельно
+sns.boxplot(x=df["Age"])
+plt.show()
