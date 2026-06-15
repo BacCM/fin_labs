b892951424e56625d291d5fa2001d3a9b0aefa68
BacCM <baccm@mail.ru>
2026-06-15 09:47:24 +0300

    lab4 part 2
    15.2
    16.1
    16.2

--- lab4_part2.py
+++ lab4_part2.py
@@ -25,6 +25,43 @@ s=df['Weight'].std() # Вычислить среднее квадратичес�
 print(s) # Вывест на экра значение СКО
 print(m-3*s,m+3*s) # Вычислить значение искомого интервала
 
-df=df[(df['Weight'] > m-3*s) & (df['Weight'] < m+3*s)]
-sns.boxplot(y=df["Weight"])
+df_w=df[(df['Weight'] > m-3*s) & (df['Weight'] < m+3*s)]
+sns.boxplot(y=df_w["Weight"])
+plt.show()
+
+# 15.2. Удалите аномальные значения параметра возраст,
+# выполнив аналогичные предыдущему заданию действия.
+
+m=df['Age'].mean()#Вычислить среднее значения (матожидание)
+print(m) # Вывести на экран значение матожидания
+s=df['Age'].std() # Вычислить среднее квадратическое отклонение
+print(s) # Вывест на экра значение СКО
+print(m-3*s,m+3*s) # Вычислить значение искомого интервала
+
+df_a=df[(df['Age'] > m-3*s) & (df['Age'] < m+3*s)]
+sns.boxplot(y=df_a["Age"])
+plt.show()
+
+# Задание 16.Удалим объекты, имеющие аномальные значения исследованных выше параметров.
+# Задание 16.1. Удалим аномальные значения параметра вес.
+# Выведем на экран значения искомых перцинтилей
+print('quantile:')
+print (df["Weight"].quantile(.25),df["Weight"].quantile(.75))
+a = df["Weight"].quantile(.25)
+b = df["Weight"].quantile(.75)
+
+df_w=df[(df['Weight'] > a-1.5*(b-a)) & (df['Weight'] < b+1.5*(b-a))]
+sns.boxplot(x=df_w['Weight'])
+plt.show()
+
+#Задание 16.2. Удалите аномальные значения параметра возраст,
+#выполнив аналогичные предыдущему заданию действия.
+
+print('quantile age:')
+print (df["Age"].quantile(.25),df["Age"].quantile(.75))
+a = df["Age"].quantile(.25)
+b = df["Age"].quantile(.75)
+
+df_a=df[(df['Age'] > a-1.5*(b-a)) & (df['Age'] < b+1.5*(b-a))]
+sns.boxplot(x=df_a['Age'])
 plt.show()
